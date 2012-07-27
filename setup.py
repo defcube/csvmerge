9e157e0df3d329a94c6bc099cee30ef6ff35a1a7
pgatt <gattster@gmail.com>
2012-07-26 17:59:50 -0700

    misc

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 from distutils.core import setup
 import os
-readme = os.path.dirname(os.path.realpath(__file__)) + \
-         open('README.rst').read()
+readme = open(os.path.dirname(os.path.realpath(__file__)) +\
+        'README.rst').read()
 setup(
     name='csvmerge',
-    version='1.0.5',
+    version='1.0.8',
     author_email='gattster@gmail.com',
     author='Philip Gatt',
     py_modules=['csvmergeutil'],
