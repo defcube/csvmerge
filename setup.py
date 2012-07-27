f3b15fed7d0cde6dfeab49be39b2c743dbebba96
pgatt <gattster@gmail.com>
2012-07-26 18:02:04 -0700

    misc

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 from distutils.core import setup
 import os
-readme = open(os.path.dirname(os.path.realpath(__file__)) +\
-        '/README.rst').read()
+try:
+    readme = open(os.path.dirname(os.path.realpath(__file__)) +\
+            '/README.rst').read()
+except IOError:
+    readme = ""
 setup(
     name='csvmerge',
-    version='1.0.8',
+    version='1.0.9',
     author_email='gattster@gmail.com',
     author='Philip Gatt',
     py_modules=['csvmergeutil'],
     scripts=['csvmerge'],
     description="Merges multiple csv files into a single file",
     long_description=readme,
-    url='http://github.com/defcube/csvmerge',
-    )
+    url='http://github.com/defcube/csvmerge')
